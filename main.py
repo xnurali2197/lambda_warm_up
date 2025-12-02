1f482088e2f3835f3f0d5187c6fc6307b60165e8
xnurali2197 <xnurali2197@gmail.com>
2025-12-02 15:08:40 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# 1
+multiply = lambda x: x * 5
+print(multiply(3))
+
+# 2
+multiply2 = lambda x, y: x*y
+print(multiply2(4, 5))
+
+# 3
+even_or_odd = lambda x: "Even" if x % 2 == 0 else "Odd"
+print(even_or_odd(7))
+
+# 4
+max_of_two = lambda x, y: x if x > y else y
+print(max_of_two(10, 20))
+
+# 5
+number_7 = lambda x: True if x > 7  else False
+print(number_7(5))
+
